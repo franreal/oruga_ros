5d090f61405af7de9b316dbff7af62729b2e9a18
Fran Real <franroyal@yahoo.es>
2014-06-03 15:04:22 +0200

    Heart-beat added and tested

---
+++ oruga_bringup/src/heartbeat.cpp
@@ -0,0 +1,39 @@
+#include <ros/ros.h>
+#include <oruga_msgs/ToOrugaData.h>
+
+class HeartBeat {
+public:
+  HeartBeat(std::string toOrugaTopic);
+  void toggle();
+  
+private:
+  ros::NodeHandle nh;
+  ros::Publisher pub;
+  oruga_msgs::ToOrugaData data;
+};
+
+HeartBeat::HeartBeat(std::string toOrugaTopic) {
+  pub = nh.advertise<oruga_msgs::ToOrugaData>(toOrugaTopic, 1);
+  data.code = 0x22;
+  data.value.push_back(0);
+}
+
+void HeartBeat::toggle() {
+  data.value[0] = !data.value[0];
+  pub.publish(data);
+}
+
+int main(int argc, char** argv) {
+
+  ros::init(argc, argv, "heartbeat");
+  HeartBeat heartbeat("oruga/command");
+
+  ros::Rate r(1);  // 1Hz
+  while (ros::ok()) {
+    
+    heartbeat.toggle();
+    
+    ros::spinOnce();
+    r.sleep();
+  }
+}
